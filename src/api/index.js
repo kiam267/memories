29a860c2f44b628ec6e2e49a146c836da53d2db8
Abid Hasan Kiam <kiamhasan267@gmail.com>
2025-04-14 00:23:48 +0600

    chore(api): update some url

--- src/api/index.js
+++ src/api/index.js
@@ -1,21 +1,24 @@
 import axios from 'axios';
+const baseURL = import.meta.env.VITE_API_URL;
+
+
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL,
   // headers: {
   //   'Content-Type': 'application/json',
   //   // Authorization: 'Bearer YOUR_JWT_TOKEN',
   // },
 });
 
-API.interceptors.request.use((req) => {
+API.interceptors.request.use(req => {
   if (localStorage.getItem('profile')) {
-    req.headers['Authorization'] = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    req.headers['Authorization'] =
+      `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
   }
-})
+});
 
-
-const url = 'http://localhost:5000/posts';
+const url = `${baseURL}/posts`;
 
 // Get Data From Server
 export const fetchPosts = () => API.get(url);
@@ -30,8 +33,10 @@ export const updatePost = (id, updatePost) =>
 // Delete Data From Server
 export const deletePost = id => API.delete(`${url}/${id}`);
 
-export const likePost = id => API.patch(`${url}/${id}/likePost`);
+export const likePost = id =>
+  API.patch(`${url}/${id}/likePost`);
 
 export const signin = fromData =>
-  API.post('http://localhost:5000/users/', fromData);
-export const signup = (fromData) => API.post("/users/signup", fromData)
+  API.post(`${baseURL}/users/`, fromData);
+export const signup = fromData =>
+  API.post('/users/signup', fromData);
